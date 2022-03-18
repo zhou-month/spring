3a1064162b15a6aeac0ffeb92b198f89e9f47c1f
zhou_moon <908143158@qq.com>
2022-03-18 09:33:35 +0800

    aop

---
+++ spring-demo/src/main/java/com/zhou/spring/demo/config/MyTestConfig.java
@@ -0,0 +1,13 @@
+package com.zhou.spring.demo.config;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Classname MyTestConfig
+ * @Description
+ * @Date 2022/3/10 16:03
+ * @Created by zhou
+ */
+@Service
+public class MyTestConfig {
+}
